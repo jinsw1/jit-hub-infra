b6ae1776ff4330a3023d443627987d7beacda611
user1 <user1@mgmt.tail192345.ts.net>
2026-07-30 00:59:44 +0900

    cicd설정

--- terraform/onprem/01-onprem-platform/01-main.tf
+++ terraform/onprem/01-onprem-platform/01-main.tf
@@ -9,7 +9,7 @@ module "cloudflared_tunnel" {
   tunnel_name             = "jit-hub-tunnel"
   domain_name              = var.domain_name
   #dns_records              = ["@", "argocd", "grafana"]
-  dns_records              = ["@", "argocd", "grafana"]
+  dns_records              = ["@"]
 
   ingress_rules = [
     # 서비스 트래픽 (평시 eks-a, 장애시 onprem, DR시 eks-b — 오리진은 replica로 스위칭)
@@ -18,14 +18,14 @@ module "cloudflared_tunnel" {
       service  = "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local:80"
     },
     # 관제용 (onprem 고정)
-    {
-      hostname = "argocd.${var.domain_name}"
-      service  = "https://argocd-server.argocd.svc.cluster.local:443"
-    },
-    {
-      hostname = "grafana.${var.domain_name}"
-      service  = "http://grafana.monitoring.svc.cluster.local:80"
-    }
+    # {
+    #   hostname = "argocd.${var.domain_name}"
+    #   service  = "https://argocd-server.argocd.svc.cluster.local:443"
+    # },
+    # {
+    #   hostname = "grafana.${var.domain_name}"
+    #   service  = "http://grafana.monitoring.svc.cluster.local:80"
+    # }
   ]
 }
 
